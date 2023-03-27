b8bf916e43a6a2bc4035ea2d9d0eadbb39a76686
fishluv <110374653+fishluv@users.noreply.github.com>
2023-03-26 22:43:05 -0700

    song page

--- pages/s/[sid]/index.tsx
+++ pages/s/[sid]/index.tsx
@@ -1,10 +1,35 @@
+import Image from "next/image"
 import { useRouter } from "next/router"
+import { Database, Chart } from "popn-db-js"
 
-const Post = () => {
+const SongPageHandler = () => {
   const router = useRouter()
   const { sid } = router.query
+  const [normalChart] = Database.findCharts(`${sid}n`)
 
-  return <p>Post: {sid}</p>
+  if (normalChart === null) {
+    return <p>Couldnt find {sid}</p>
+  } else {
+    return SongPage(normalChart)
+  }
 }
 
-export default Post
+const SongPage = (normalChart: Chart) => {
+  const { title, songId } = normalChart
+  const paddedId = `000${songId}`.slice(-4)
+  const bannerUrl = `https://popn-assets.surge.sh/kc_${paddedId}.png`
+
+  return (
+    <>
+      <Image
+        src={bannerUrl}
+        alt={`banner for ${title}`}
+        width={160}
+        height={44}
+      />
+      <h1>{title}</h1>
+    </>
+  )
+}
+
+export default SongPageHandler
