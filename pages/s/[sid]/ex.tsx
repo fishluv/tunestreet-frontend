52741d36d2ab590f15a0cda47b64e17f8404932d
fishluv <110374653+fishluv@users.noreply.github.com>
2023-03-28 03:39:54 -0700

    implement fetch rating

--- pages/s/[sid]/ex.tsx
+++ pages/s/[sid]/ex.tsx
@@ -1,8 +1,31 @@
+import useSWR from "swr"
 import Image from "next/image"
 import { useRouter } from "next/router"
 import { Database, Chart } from "popn-db-js"
 
-const ChartPageHandler = () => {
+const urlFetcher = (url: string) => fetch(url).then((res) => res.json())
+
+function MyRating() {
+  const router = useRouter()
+  const { sid } = router.query
+  const url =
+    "https://backend.tunestreet.org/rating?" +
+    new URLSearchParams({
+      type: "quality",
+      entity_type: "chart",
+      entity_id: `${sid}ex`,
+    })
+  const { data, error } = useSWR(url, urlFetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
+  const value = data.rating?.value
+
+  return <h2>Rating: {value ?? "none"}</h2>
+}
+
+function ChartPageHandler() {
   const router = useRouter()
   const { sid } = router.query
   const [chart] = Database.findCharts(`${sid}ex`)
@@ -14,7 +37,7 @@ const ChartPageHandler = () => {
   }
 }
 
-const ChartPage = (chart: Chart) => {
+function ChartPage(chart: Chart) {
   const { title, difficulty, level, songId } = chart
   const paddedId = `000${songId}`.slice(-4)
   const bannerUrl = `https://popn-assets.surge.sh/kc_${paddedId}.png`
@@ -31,6 +54,7 @@ const ChartPage = (chart: Chart) => {
       <h2>
         {difficulty} {level}
       </h2>
+      <MyRating />
     </>
   )
 }
