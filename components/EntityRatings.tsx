d9e5499e313b8822496323cca6ce381eb0561245
fishluv <110374653+fishluv@users.noreply.github.com>
2023-04-08 22:59:10 -0700

    entice login for my ratings

--- components/EntityRatings.tsx
+++ components/EntityRatings.tsx
@@ -3,14 +3,34 @@ import StarBar from "@/components/StarBar"
 import { useState } from "react"
 import styles from "@/components/StarBar.module.scss"
 import getBackendUrl from "@/getBackendUrl"
+import Link from "next/link"
 
 export interface EntityOptions {
   entityType: "song" | "chart"
   entityId: string
 }
 
-const urlFetcher = (url: string) =>
-  fetch(url, { credentials: "include" }).then((res) => res.json())
+class HttpError extends Error {
+  data: any
+  status: number
+
+  constructor(data: any, status: number) {
+    super("Error")
+    this.data = data
+    this.status = status
+  }
+}
+
+const urlFetcher = async (url: string) => {
+  const res = await fetch(url, { credentials: "include" })
+
+  if (!res.ok) {
+    const data = await res.json()
+    throw new HttpError(data, res.status)
+  }
+
+  return res.json()
+}
 
 function SiteRatings({ entityType, entityId }: EntityOptions) {
   const url = getBackendUrl(
@@ -19,12 +39,12 @@ function SiteRatings({ entityType, entityId }: EntityOptions) {
   )
   const { data, error } = useSWR(url, urlFetcher)
 
-  if (error) return <div>Failed to load</div>
+  if (error?.data) return <div>Error loading site ratings {error.data}</div>
   if (!data) return <div>Loading...</div>
 
-  const { site_entity_ratings, errors } = data
+  const { site_entity_ratings } = data
   if (!site_entity_ratings) {
-    return <div>Failed to load {errors ?? errors}</div>
+    return <div>Couldnt find site ratings data</div>
   }
 
   const { quality_rating, difficulty_rating } = site_entity_ratings
@@ -45,12 +65,20 @@ function MyRatings({ entityType, entityId }: EntityOptions) {
   )
   const { data, error } = useSWR(url, urlFetcher)
 
-  if (error) return <div>Failed to load</div>
+  if (error?.status === 401) {
+    return (
+      <div>
+        <Link href="/login">Log in</Link> to rate this {entityType}
+      </div>
+    )
+  }
+
+  if (error?.data) return <div>Error loading your ratings {error.data}</div>
   if (!data) return <div>Loading...</div>
 
-  const { user_entity_ratings, errors } = data
+  const { user_entity_ratings } = data
   if (!user_entity_ratings) {
-    return <div>Failed to load {errors ?? errors}</div>
+    return <div>Couldnt find your ratings data</div>
   }
 
   const { quality_rating, difficulty_rating } = user_entity_ratings
@@ -60,6 +88,7 @@ function MyRatings({ entityType, entityId }: EntityOptions) {
       <h2>My ratings:</h2>
       <h3>Quality: {quality_rating ?? "n/a"}</h3>
       <h3>Difficulty: {difficulty_rating ?? "n/a"}</h3>
+      <QualityRater />
     </div>
   )
 }
@@ -83,7 +112,6 @@ export default function EntityRatings({ entityType, entityId }: EntityOptions) {
     <>
       <SiteRatings entityType={entityType} entityId={entityId} />
       <MyRatings entityType={entityType} entityId={entityId} />
-      <QualityRater />
     </>
   )
 }
