8120be70a617eedc7d61e3ecde254e95824ac2f5
fishluv <110374653+fishluv@users.noreply.github.com>
2023-04-09 23:27:08 -0700

    image star bar

--- components/StarBar.tsx
+++ components/StarBar.tsx
@@ -1,6 +1,41 @@
 import { ReactNode, useState } from "react"
 import clsx from "clsx"
 import styles from "./StarBar.module.scss"
+import starSprite from "@/assets/star_sprite.png" // Stolen from Rate Your Music.
+
+function StarImageBar({
+  sizeMultiplier,
+  valueInHalves,
+  setValueInHalves,
+}: {
+  sizeMultiplier: number
+  valueInHalves: number
+  setValueInHalves(valueInHalves: number): void
+}) {
+  const widthPx = 90 * sizeMultiplier
+  const heightPx = 16 * sizeMultiplier
+  const spriteDistancePx = 66 * sizeMultiplier
+
+  let spriteIndex
+  if (valueInHalves === 0) {
+    spriteIndex = 0
+  } else if (valueInHalves === 10) {
+    spriteIndex = 1
+  } else {
+    spriteIndex = valueInHalves + 1
+  }
+  const bgPosYPx = spriteIndex * spriteDistancePx * -1
+
+  const styleObj = {
+    width: `${widthPx}px`,
+    height: `${heightPx}px`,
+    backgroundImage: `url(${starSprite.src})`,
+    backgroundPositionX: "left",
+    backgroundPositionY: `${bgPosYPx}px`,
+    backgroundSize: "cover",
+  }
+  return <div style={styleObj}></div>
+}
 
 function StarHalf({
   side,
@@ -48,5 +83,14 @@ export default function StarBar({
     return stars
   }
 
-  return <span>{prepareStars()}</span>
+  return (
+    <span>
+      {prepareStars()}
+      <StarImageBar
+        sizeMultiplier={1.5}
+        valueInHalves={valueInHalves}
+        setValueInHalves={setValueInHalves}
+      />
+    </span>
+  )
 }
