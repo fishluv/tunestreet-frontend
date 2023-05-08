2d4ee2aade7bd58ef4a4f80b4840bd31a49b0625
fishluv <110374653+fishluv@users.noreply.github.com>
2023-05-08 00:15:13 -0700

    allow updating username

--- components/MePage.tsx
+++ components/MePage.tsx
@@ -1,6 +1,93 @@
 import Head from "next/head"
 import { useAuth } from "@/components/authentication"
 import Link from "next/link"
+import { ChangeEvent, KeyboardEvent, useState } from "react"
+import { getBackendUrl } from "@/lib/backendUrls"
+
+function UsernameUpdate({ initialUsername }: { initialUsername: string }) {
+  const [confirmedUsername, setConfirmedUsername] = useState(initialUsername)
+  const [pendingUsername, setPendingUsername] = useState(initialUsername)
+  const [isEditing, setIsEditing] = useState(false)
+  const [isConfirming, setIsConfirming] = useState(false)
+  const [confirmResult, setConfirmResult] = useState("")
+
+  function onUsernameInputChange(e: ChangeEvent<HTMLInputElement>) {
+    setPendingUsername(e.target.value)
+  }
+
+  function onUsernameEditButtonClick() {
+    setIsEditing(true)
+    setConfirmResult("")
+  }
+
+  function onUsernameInputKeyUp(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      onUsernameConfirmButtonClick()
+    }
+  }
+
+  function onUsernameConfirmButtonClick() {
+    if (isConfirming) {
+      return
+    }
+
+    setIsConfirming(true)
+    setConfirmResult("")
+
+    fetch(getBackendUrl("/users/me"), {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: pendingUsername,
+        },
+      }),
+    }).then((res) => {
+      setConfirmResult(res.status === 200 ? "Success!" : "Failed. Try again.")
+      setIsConfirming(false)
+      setConfirmedUsername(pendingUsername)
+      setIsEditing(false)
+    })
+  }
+
+  function onUsernameCancelButtonClick() {
+    setIsEditing(false)
+    setPendingUsername(confirmedUsername)
+  }
+
+  if (isEditing) {
+    return (
+      <>
+        Username:{" "}
+        <input
+          type="text"
+          value={pendingUsername}
+          onChange={onUsernameInputChange}
+          onKeyUp={onUsernameInputKeyUp}
+          width={32}
+          disabled={isConfirming}
+        />
+        <button onClick={onUsernameConfirmButtonClick} disabled={isConfirming}>
+          Confirm
+        </button>
+        <button onClick={onUsernameCancelButtonClick} disabled={isConfirming}>
+          Cancel
+        </button>
+      </>
+    )
+  } else {
+    return (
+      <>
+        Username: {confirmedUsername}{" "}
+        <button onClick={onUsernameEditButtonClick}>Edit</button>
+        {confirmResult}
+      </>
+    )
+  }
+}
 
 export default function MePage() {
   const auth = useAuth()
@@ -12,9 +99,9 @@ export default function MePage() {
     return (
       <>
         <Head>
-          <title>Update my info • TuneStreet</title>
+          <title>My info • TuneStreet</title>
         </Head>
-        Username: {username}
+        <UsernameUpdate initialUsername={username ?? ""} />
       </>
     )
   } else {
